aa9233f28dea997981545ec79bff4b1167bb34e2
Taras Tymchyshyn <tarast@livv.com>
2023-05-10 15:51:03 +0200

    add basic scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,63 @@
+import 'package:film_riverpod_test/model/search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(
+    child: FilmApp(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FilmApp extends ConsumerWidget {
+  const FilmApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ThemeData();
+    final films = <Search>[Search(title: 'StarWars', year: '1990')];
+
+    return MaterialApp(
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.black,
+          secondary: Colors.blue,
+        ),
+      ),
+      title: 'FilmApp',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Film title:',
+                  ),
+                  onChanged: (text) async {},
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: films.length,
+                    itemBuilder: (context, index) {
+                      final film = films[index];
+
+                      return Container(
+                        child: Text(film.title!),
+                      );
+                      // MovieCard(movie: movie);
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
